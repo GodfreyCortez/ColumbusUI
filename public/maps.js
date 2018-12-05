cba4a0abf2cdb3c96bc8f31447b21058a912d2af
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-12-05 14:58:32 -0500

    Add opt row

--- public/maps.js
+++ public/maps.js
@@ -15,7 +15,12 @@ const reqRow = `
                 <input type="text" class="form-control" placeholder="Time" />
               </div>
               <div class="col">
-                <button type="submit" class="Remove -">Remove</button>
+                <button class="remove">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path fill="none" d="M0 0h24v24H0z"/>
+                      <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 10H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"/>
+                    </svg>
+                </button>
               </div>
             </div>
 `
@@ -26,7 +31,12 @@ const optRow = `
                 <input type="text" class="form-control" placeholder="Location" class="location"/>
               </div>
               <div class="col">
-                <button type="submit" class="remove">Remove -</button>
+                <button class="remove">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path fill="none" d="M0 0h24v24H0z"/>
+                      <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 10H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"/>
+                    </svg>
+                </button>
               </div>
             </div>
 `
@@ -34,9 +44,6 @@ const optRow = `
 $(document).ready(() => {
   $("#opt-add").click(() => {
     console.log("clicked opt-add")
-    $("#opt-input-group").append(
-    `<input type="text" placeholder="location" class="form-control"/>
-      <button>Delete -</button>`
-  )
+    $("#opt-input-group").append(optRow)
   })
 })
