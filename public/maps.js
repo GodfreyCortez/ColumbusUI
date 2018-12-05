22303986609a3ffd3f3dce6664bdda0df71dbeef
Nicole Jia <nicole.jia@scotiabank.com>
2018-12-05 16:33:40 -0500

    Fixed the css

--- public/maps.js
+++ public/maps.js
@@ -15,7 +15,7 @@ const reqRow = `
                 <input type="text" class="form-control time" placeholder="Time" />
               </div>
               <div class="col">
-                <div class="remove" onclick="return deleteRow(this);">
+                <div class="button" onclick="return deleteRow(this);">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                       <path fill="none" d="M0 0h24v24H0z"/>
                       <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 10H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"/>
@@ -28,10 +28,10 @@ const reqRow = `
 const optRow = `
             <div class="form-row location-row">
               <div class="col">
-                <input type="text" class="form-control location" placeholder="Location" />
+                <input type="text" class="form-control optInput location" placeholder="Location" />
               </div>
               <div class="col">
-                <div class="remove">
+                <div class="button buttonOpt">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                       <path fill="none" d="M0 0h24v24H0z"/>
                       <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-3 10H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"/>
