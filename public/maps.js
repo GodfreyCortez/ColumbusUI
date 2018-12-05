981ea81ed63252317beb254e1a4d5048cafe6a57
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-12-05 15:01:35 -0500

    req row only specified in js

--- public/maps.js
+++ public/maps.js
@@ -42,10 +42,13 @@ const optRow = `
 `
 
 $(document).ready(() => {
+  $("#req-input-group").append(reqRow)
+
   $("#req-add").click(() => {
     console.log("clicked req-add")
     $("#req-input-group").append(reqRow)
   })
+
   $("#opt-add").click(() => {
     console.log("clicked opt-add")
     $("#opt-input-group").append(optRow)
